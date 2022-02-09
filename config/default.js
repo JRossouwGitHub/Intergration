17798d75508444c6c2e43ce2e40056f60a24f833
JRossouwGitHub <jrdesigns.work@gmail.com>
2022-02-10 10:01:08 +1300

    Commit

--- config/default.js
+++ config/default.js
@@ -1,3 +1,4 @@
 module.exports = {
-    xeroClientID: '13770C3BA00F4909AB84FCECAAFE1ACD'
+    xeroClientID: '13770C3BA00F4909AB84FCECAAFE1ACD',
+    xeroClientSecret: 'qHdljuJ2oqfmS0uTcoscF9m8UVpBgDF5cQQjyahvuy9KPnSp'
 }
