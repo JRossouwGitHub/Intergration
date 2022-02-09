1bc0d3eca3860cbea028d94c6afc87a69da7d90b
JRossouwGitHub <jrdesigns.work@gmail.com>
2022-02-10 09:30:30 +1300

    Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 const xero = require('./routes/api/xero')
 
 app.use('/api/xero', xero)
