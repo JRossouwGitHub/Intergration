cc9be1c99e4fb1cec13038aa91bbc814a034f230
JRossouwGitHub <jrdesigns.work@gmail.com>
2022-02-10 09:47:23 +1300

    Commit

---
+++ config/default.js
@@ -0,0 +1,3 @@
+module.exports = {
+    xeroClientID: '13770C3BA00F4909AB84FCECAAFE1ACD'
+}
--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const xero = require('./routes/api/xero')
+const config = require('./config')
 
 app.use('/api/xero', xero)
 
 app.get('/', (req, res) => {
     res.send('Intergration software running!')
+})
+
+app.get('/login', (req, res) => {
+    res.send({data: config})
+    //res.redirect('https://login.xero.com/identity/connect/authorize?response_type=code&client_id=YOURCLIENTID&redirect_uri=YOURREDIRECTURI&scope=openid profile email accounting.transactions&state=123')
 })
 
 //Redirect after connecting to XERO and getting data
