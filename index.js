82ed709ab10033ffd59a47b5f703a1dd66c35851
JRossouwGitHub <jrdesigns.work@gmail.com>
2022-02-10 09:28:26 +1300

    Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000 || process.env.PORT
 const xero = require('./routes/api/xero')
 
 app.use('/api/xero', xero)
@@ -8,6 +8,11 @@ app.use('/api/xero', xero)
 app.get('/', (req, res) => {
     res.send('Intergration software running!')
 })
+
+//Redirect after connecting to XERO and getting data
+app.get('/redirect', (req, res) => {
+    res.send('Worked!');
+});
 
 app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`)
