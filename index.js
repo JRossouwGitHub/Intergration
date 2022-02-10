7a50d22bd545387bb2b2fbcf41a4f27ae04d6c4e
JRossouwGitHub <jrdesigns.work@gmail.com>
2022-02-10 14:13:26 +1300

    Commit

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.get('/redirect', (req, res) => {
             code: req.query.code,
             redirect_uri: 'https://infusionxero.herokuapp.com/redirect'
         }
-        res.redirect(`/api/xero/token?grant_type=${grant_type}&code=${body.code}&redirect_uri=${body.redirect_uri}`)
+        res.redirect(`/api/xero/token?grant_type=${body.grant_type}&code=${body.code}&redirect_uri=${body.redirect_uri}`)
     } catch(e){
         console.log(e)
-        res.send('Success! You have been logged in for 30min.');
+        res.send('Error! There was an issue authenticating your login.');
     }
 });
 
