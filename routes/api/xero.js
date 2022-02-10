051ccae1a0a1dc346ed2a91a7392ece48d0d1b45
JRossouwGitHub <jrdesigns.work@gmail.com>
2022-02-11 08:33:07 +1300

    Commit

--- routes/api/xero.js
+++ routes/api/xero.js
@@ -14,55 +14,28 @@ Router.get('/login', (req, res) => {
 
 Router.get('/token', (req, res) => {
     const base64data = Buffer.from(`${config.xeroClientID}:${config.xeroClientSecret}`).toString('base64')
-    // const data = {
-    //     authorization_code: req.query.grant_type,
-    //     code: req.query.code,
-    //     redirect_uri: req.query.redirect_uri
-    // }
-    // var options = {
-    //     headers: {
-    //         'authorization': "Basic " + base64data,
-    //         'Content-Type': 'application/x-www-form-urlencoded'
-    //     },
-    //     method: 'POST',
-    //     body: {
-    //         grant_type: data.authorization_code,
-    //         code: data.code,
-    //         redirect_uri: data.redirect_uri
-    //     },
-    //     uri: 'https://identity.xero.com/connect/token'
-    // }
-    // request(options, function (error, response) {
-    //     if(!error){
-    //         res.send('Getting Token')
-    //     } else {
-    //         console.log(error)
-    //         res.send({err: error})
-    //     }
-    // });
     let _body = JSON.stringify({
         'grant_type': req.query.grant_type,
         'code': req.query.code,
         'redirect_uri': req.query.redirect_uri
     })
-    request({
-        url : 'https://identity.xero.com/connect/token',
-        method :"POST",
-        headers : {
+    var options = {
+        headers: {
             'authorization': "Basic " + base64data,
             'Content-Type': 'application/x-www-form-urlencoded'
         },
+        method: 'POST',
         body: _body,
-        json: true
-      },
-      function(){
+        uri: 'https://identity.xero.com/connect/token'
+    }
+    request(options, function (error, response) {
         if(!error){
             res.send('Getting Token')
         } else {
             console.log(error)
             res.send({err: error})
         }
-      })
+    });
 })
 
 module.exports = Router;
